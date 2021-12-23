0e3650ae6743535364456588f40f24c08a2c94b2
Joshua Bracken <84690420+halfrussian@users.noreply.github.com>
2021-12-23 12:10:04 -0600

    added more important tables

--- data.js
+++ data.js
@@ -3,6 +3,13 @@ Table users {
   created_at TIMESTAMP
   updated_at TIMESTAMP
   username varchar(30)
+  bio varchar(400)
+  avatar varchar(200)
+  phone varchar(25)
+  email varchar(40)
+  password varchar(50)
+  status varchar(15)
+  verified boolean
 }
 
 Table posts {
@@ -32,3 +39,42 @@ Table likes {
   comment_id INTEGER [ref: > comments.id]
   post_id INTEGER [ref: > posts.id]
 }
+
+Table photo_tags {
+  id SERIAL [pk, increment]
+  created_at TIMESTAMP 
+  updated_at TIMESTAMP
+  post_id INTEGER [ref: > posts.id]
+  user_id INTEGER [ref: > users.id]
+  x INTEGER
+  y INTEGER
+}
+
+Table caption_tags {
+  id SERIAL [pk, increment]
+  created_at TIMESTAMP
+  post_id INTEGER [ref: > posts.id]
+  user_id INTEGER [ref: > users.id]
+}
+
+Table hashtags {
+  id SERIAL [pk, increment]
+  created_at TIMESTAMP
+  title VARCHAR(20)
+}
+
+Table hashtags_posts {
+  id SERIAL [pk, increment]
+  hashtag_id INTEGER [ref: > hashtags.id]
+  post_id INTEGER [ref: > posts.id]
+  
+}
+
+Table followers {
+  id SERIAL [pk, increment]
+  created_at TIMESTAMP
+  leader_id INTEGER [ref: > users.id]
+  follower_id INTEGER [ref: > users.id]
+}
+
+
