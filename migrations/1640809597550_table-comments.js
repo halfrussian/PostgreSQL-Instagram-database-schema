085c98539aa69445912b4827462ff94be090e940
Joshua Bracken <joshua.bracken@kingmetals.com>
2021-12-29 15:42:14 -0600

    adding node schema migrations

---
+++ migrations/1640809597550_table-comments.js
@@ -0,0 +1,21 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.sql(`
+        CREATE table comments (
+            id SERIAL PRIMARY KEY,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            contents VARCHAR(240) NOT NULL 
+        );
+    `)
+};
+
+exports.down = pgm => { 
+    pgm.sql(`
+        DROP TABLE comments;
+    `)
+};
+
